c51e38d48af404eb13a748bd30ad0ad2bfe34a9b
Alexandr <Gsashko@mail.ru>
2012-10-27 12:38:39 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PullRequest::Application.config.secret_token = '57f9e45c91e121b846ce42c3a9f9be13b17350deef5ad7a0b1a4e3ee28f3722614478a5522e940438e3d8813c7886feacd7757387843ce18fd679fec9c936c47'
